3bb72eeb25db2e15c8c66c15b0989ed4a0af84d9
NicolasJamar <nicolas.jamar@gmail.com>
2025-02-18 18:47:35 +0100

    add logic to update-student

--- update-student.php
+++ update-student.php
@@ -10,42 +10,49 @@ require_once "connexion.php";
 session_start();
 
 
-if(!empty($_POST)) {
+if(!empty($_POST) OR !empty($_GET["id"])) {
     // 1. Check all the inputs exist
     // 2. We check also if the $_POST are not empty because we load the page, the form is empty
-    if(isset($_POST["firstname"],$_POST["lastname"],$_POST["promo"])
-        && !empty($_POST["firstname"]) && !empty($_POST["lastname"]) && !empty($_POST["promo"])) {
+    if(isset($_GET['id'])) {
+        $id_student = (int) $_GET["id"];
 
-        $firstname = strip_tags($_POST["firstname"]);
-        $lastname = strip_tags($_POST["lastname"]);
-        $id_promo = (int) $_POST["promo"];
+        $q = $db->prepare("SELECT * FROM students WHERE id = :id");
+        $q->bindParam(":id", $id_student, PDO::PARAM_INT);
+        $q->execute();
+        $student = $q->fetch();
 
-        //SQL part
-        $q = $db->prepare("UPDATE students SET firstname= :firstname, lastname= :lastname, ID_promo= :id_promo");
+    if(isset($_POST["firstname"]) OR isset($_POST["lastname"]) OR isset($_POST["promo"])) {
 
-        // bindParam() accepte uniquement une variable qui est interprétée au moment de l'execute()
-        $q->bindParam(":firstname", $firstname, PDO::PARAM_STR);
-        $q->bindParam(":lastname", $lastname, PDO::PARAM_STR);
-        $q->bindParam(":id_promo", $id_promo, PDO::PARAM_INT);
+      $firstname = strip_tags($_POST["firstname"]);
+      $lastname = strip_tags($_POST["lastname"]);
+      $id_promo = (int)$_POST["promo"];
 
-        // exexute return a boolean
-        if(!$q->execute()) {
-            die("form not sent to the db");
-        }
+      if ($firstname === "") {
+        $firstname = $student["firstname"];
+      }
+      if ($lastname === "") {
+        $lastname = $student["lastname"];
+      }
 
-        header("location: student.php?id=$_GET[id]");
-        exit;
+      //SQL part
+      $q = $db->prepare("UPDATE students SET firstname= :firstname, lastname= :lastname, ID_promo= :id_promo WHERE ID=$id_student");
+
+      // bindParam() accepte uniquement une variable qui est interprétée au moment de l'execute()
+      $q->bindParam(":firstname", $firstname, PDO::PARAM_STR);
+      $q->bindParam(":lastname", $lastname, PDO::PARAM_STR);
+      $q->bindParam(":id_promo", $id_promo, PDO::PARAM_INT);
+
+      // exexute return a boolean
+      if (!$q->execute()) {
+        die("form not sent to the db");
+      }
+
+      header("location: student.php?id=$_GET[id]");
+      exit;
 
     }
-}
 
-if(isset($_GET["id"])) {
-    $id = $_GET["id"];
-
-    $q = $db->prepare("SELECT * FROM students WHERE id = :id");
-    $q->bindParam(":id", $id, PDO::PARAM_INT);
-    $q->execute();
-    $student = $q->fetch();
+    }
 }
 
 // Request for promos
